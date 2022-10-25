b1695324f5ba8d6023e12c84be9dc3134b421582
Kimsooyeun <kimsy970305@gmail.com>
2022-10-25 09:10:41 +0900

    20221025 [9:09]  rlatndus [jsp] index.jsp - <button class="conBtn" id="btn" onclick="clickCounter()"></button> 를 <button class="conBtn" id="btn" onclick="clickCounter(this)"></button> 로 변경 ( 추후 c:forEach 쓸 것에 대비)
    boardList.jsp -
    <button class="mselect" id="mselect" onclick="importantClick()"></button> 를 <button class="mselect" id="mselect" onclick="importantClick(this)"></button> 로 변경 ( 추후 c:forEach 쓸 것에 대비)
    
    [js]
    main.js
    - let cnt = 0;
    const button = document.getElementById("btn");
    function clickCounter(e){
     var btn = $(e).attr('class');
        cnt+=1;
        if((cnt % 2) === 1){
     $(e).parent().siblings('.conHover').addClass('run');
         $(e).parent().siblings('.conHover').children().css("display","block");
        } else{
          $(e).parent().siblings('.conHover').removeClass("run");
          $(e).parent().siblings('.conHover').children().css("display","none");
        }
    }
    
    const btn = document.getElementById("mselect");
    function importantClick(b){
     var btn = $(b).attr('class');
      cnt+=1;
      if(cnt % 2 === 1){
        $(b).addClass('important')
      } else{
        $(b).removeClass('important')
      }
    }

--- WebContent/resources/js/main.js
+++ WebContent/resources/js/main.js
@@ -1,26 +1,25 @@
-
 let cnt = 0;
 const button = document.getElementById("btn");
-function clickCounter(){
+function clickCounter(e){
+	var btn = $(e).attr('class');
     cnt+=1;
-    if(cnt % 2 === 1){
-      $("#btn").parents('div').siblings('.conHover').addClass('run')
-      $('.del').css("display","block")
-      $('.reWrite').css("display","block")
+    if((cnt % 2) === 1){
+	$(e).parent().siblings('.conHover').addClass('run');
+     $(e).parent().siblings('.conHover').children().css("display","block");
     } else{
-      $(".conHover").removeClass("run");
-      $('.del').css("display","none")
-      $('.reWrite').css("display","none")
+      $(e).parent().siblings('.conHover').removeClass("run");
+      $(e).parent().siblings('.conHover').children().css("display","none");
     }
 }
 
 const btn = document.getElementById("mselect");
-function importantClick(){
+function importantClick(b){
+	var btn = $(b).attr('class');
   cnt+=1;
   if(cnt % 2 === 1){
-    $('.mselect').addClass('important')
+    $(b).addClass('important')
   } else{
-    $('.mselect').removeClass('important')
+    $(b).removeClass('important')
   }
 }
 
